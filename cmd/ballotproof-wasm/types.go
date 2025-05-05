c46f0b0d0a4208d1a14040b20543bcb7f3df1c57
p4u <pau@dabax.net>
2025-05-05 17:58:29 +0200

    ballotproof: modify WASM output to return the formated Ballot
    Signed-off-by: p4u <pau@dabax.net>

--- cmd/ballotproof-wasm/types.go
+++ cmd/ballotproof-wasm/types.go
@@ -6,6 +6,7 @@ package main
 import (
 	"syscall/js"
 
+	"github.com/vocdoni/vocdoni-z-sandbox/crypto/elgamal"
 	"github.com/vocdoni/vocdoni-z-sandbox/types"
 )
 
@@ -30,26 +31,26 @@ func (i *BallotProofWasmInputs) Unmarshal(v js.Value) error {
 }
 
 type CircomInputs struct {
-	Fields           []string       `json:"fields"`
-	MaxCount         string         `json:"max_count"`
-	ForceUniqueness  string         `json:"force_uniqueness"`
-	MaxValue         string         `json:"max_value"`
-	MinValue         string         `json:"min_value"`
-	MaxTotalCost     string         `json:"max_total_cost"`
-	MinTotalCost     string         `json:"min_total_cost"`
-	CostExp          string         `json:"cost_exp"`
-	CostFromWeight   string         `json:"cost_from_weight"`
-	Address          string         `json:"address"`
-	Weight           string         `json:"weight"`
-	ProcessID        string         `json:"process_id"`
-	PK               []string       `json:"pk"`
-	K                string         `json:"k"`
-	CipherFields     []string       `json:"cipherfields"`
-	Nullifier        string         `json:"nullifier"`
-	Commitment       string         `json:"commitment"`
-	Secret           string         `json:"secret"`
-	InputsHash       types.HexBytes `json:"inputs_hash"`
-	InputsHashBigInt string         `json:"inputs_hash_bigint"`
+	Fields           []string        `json:"fields"`
+	MaxCount         string          `json:"maxCount"`
+	ForceUniqueness  string          `json:"forceUniqueness"`
+	MaxValue         string          `json:"maxValue"`
+	MinValue         string          `json:"minValue"`
+	MaxTotalCost     string          `json:"maxTotalCost"`
+	MinTotalCost     string          `json:"minTotalCost"`
+	CostExp          string          `json:"costExp"`
+	CostFromWeight   string          `json:"costFromWeight"`
+	Address          string          `json:"address"`
+	Weight           string          `json:"weight"`
+	ProcessID        string          `json:"processId"`
+	PK               []string        `json:"pk"`
+	K                string          `json:"k"`
+	Ballot           *elgamal.Ballot `json:"ballot"`
+	Nullifier        string          `json:"nullifier"`
+	Commitment       string          `json:"commitment"`
+	Secret           string          `json:"secret"`
+	InputsHash       types.HexBytes  `json:"inputsHash"`
+	InputsHashBigInt string          `json:"inputsHashBigInt"`
 }
 
 type BallotProofWasmResult struct {
