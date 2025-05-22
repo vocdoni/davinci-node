e023ea1b5a52f7522f5de41c1c43ed312795f17f
Lucas Menendez <hi@lucasmenendez.me>
2025-05-22 09:17:23 +0200

    cast votes and wait for be registered in the smart contract

--- cmd/e2e-test/main.go
+++ cmd/e2e-test/main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"math/big"
 	"net/http"
 	"time"
 
-	flag "github.com/spf13/pflag"
-
 	"github.com/ethereum/go-ethereum/common"
+	ethcrypto "github.com/ethereum/go-ethereum/crypto"
+	flag "github.com/spf13/pflag"
 	"github.com/vocdoni/arbo/memdb"
 	"github.com/vocdoni/vocdoni-z-sandbox/api"
 	"github.com/vocdoni/vocdoni-z-sandbox/api/client"
 	"github.com/vocdoni/vocdoni-z-sandbox/circuits"
+	"github.com/vocdoni/vocdoni-z-sandbox/circuits/ballotproof"
+	ballotprooftest "github.com/vocdoni/vocdoni-z-sandbox/circuits/test/ballotproof"
 	"github.com/vocdoni/vocdoni-z-sandbox/config"
+	"github.com/vocdoni/vocdoni-z-sandbox/crypto/elgamal"
 	"github.com/vocdoni/vocdoni-z-sandbox/crypto/signatures/ethereum"
 	"github.com/vocdoni/vocdoni-z-sandbox/log"
 	"github.com/vocdoni/vocdoni-z-sandbox/sequencer"
@@ -58,24 +62,20 @@ func main() {
 		privKey           = flag.String("privkey", testLocalAccountPrivKey, "private key to use for the Ethereum account")
 		deployContracts   = flag.Bool("deployContracts", false, "define if contracts should be deployed or not, if not, it will use the ones already deployed and defined in config/contracts.go")
 		web3rpcs          = flag.StringSlice("web3rpcs", nil, "web3 rpc http endpoints")
-		testTimeout       = flag.Duration("timeout", 10*time.Minute, "timeout for the test")
+		testTimeout       = flag.Duration("timeout", 20*time.Minute, "timeout for the test")
 		sequencerEndpoint = flag.String("sequencerEndpoint", defaultSequencerEndpoint, "sequencer endpoint")
 		createOrg         = flag.Bool("createOrganization", true, "create a new organization")
-		// voteCount         = flag.Int("voteCount", 10, "number of votes to cast")
-		// voteSleepTime     = flag.Duration("voteSleepTime", 10*time.Second, "time to sleep between votes")
+		voteCount         = flag.Int("voteCount", 10, "number of votes to cast")
+		voteSleepTime     = flag.Duration("voteSleepTime", 10*time.Second, "time to sleep between votes")
 	)
 	flag.Parse()
-
 	log.Init("debug", "stdout", nil)
-
+	// Create a context with the test timeout
 	testCtx, cancel := context.WithTimeout(context.Background(), *testTimeout)
 	defer cancel()
 
-	var err error
-	contracts := &web3.Contracts{}
-	*privKey = util.TrimHex(*privKey)
-
 	// If no web3rpcs are provided, use the default ones from chainlist
+	var err error
 	if len(*web3rpcs) == 0 {
 		if *web3rpcs, err = chainlist.EndpointList(defaultNetwork, 10); err != nil {
 			log.Fatal(err)
@@ -83,6 +83,7 @@ func main() {
 	}
 	// If the contracts should be deployed, deploy them, if not use the ones
 	// already deployed and defined in config/contracts.go
+	var contracts *web3.Contracts
 	if *deployContracts {
 		log.Fatalf("deploying contracts is not supported yet")
 		return
@@ -116,27 +117,8 @@ func main() {
 
 	// If no sequencer endpoint is provided, start a local one
 	if *sequencerEndpoint == defaultSequencerEndpoint {
-		// Create storage with a in-memory database
-		stg := storage.New(memdb.New())
-		sequencer.AggregatorTickerInterval = time.Second * 2
-		sequencer.NewProcessMonitorInterval = time.Second * 5
-		vp := service.NewSequencer(stg, contracts, time.Second*30)
-		if err := vp.Start(testCtx); err != nil {
-			log.Fatal(err)
-		}
-		// Monitor new processes from the contracts
-		pm := service.NewProcessMonitor(contracts, stg, time.Second*2)
-		if err := pm.Start(testCtx); err != nil {
-			log.Fatal(err)
-		}
-		// Start finalizer service
-		fin := service.NewFinalizer(stg, stg.StateDB(), time.Second*5)
-		if err := fin.Start(testCtx, time.Second*5); err != nil {
-			log.Fatal(err)
-		}
-		// Start API service
-		api := service.NewAPI(stg, defaultSequencerHost, defaultSequencerPort, defaultNetwork)
-		if err := api.Start(testCtx); err != nil {
+		log.Infow("no remote sequencer endpoint provided, starting a local one...")
+		if err := startLocalSequencer(testCtx, contracts); err != nil {
 			log.Fatal(err)
 		}
 	}
@@ -177,10 +159,13 @@ func main() {
 	}
 
 	// Create a new census with numBallot participants
-	censusRoot, participants, signers, err := createCensus(cli, 10)
+	censusRoot, signers, err := createCensus(cli, *voteCount)
 	if err != nil {
 		log.Errorw(err, "failed to create census")
 	}
+	log.Infow("census created", 
+		"root", hex.EncodeToString(censusRoot), 
+		"participants", len(signers))
 
 	// Create a new process with mocked ballot mode
 	pid, encryptionKey, err := createProcess(testCtx, contracts, cli, censusRoot, mockedBallotMode)
@@ -190,10 +175,85 @@ func main() {
 	}
 	log.Infow("process created", "pid", pid.String())
 
-	// TODO: remove this
-	_ = participants
-	_ = signers
-	_ = encryptionKey
+	// Generate votes for each participant and send them to the sequencer
+	for i, signer := range signers {
+		// Generate a vote for each participant
+		vote, err := createVote(signer, pid, encryptionKey, &mockedBallotMode)
+		if err != nil {
+			log.Errorw(err, "failed to create vote")
+			return
+		}
+		log.Infow("vote created", "vote", vote)
+
+		// Generate a census proof for each participant
+		vote.CensusProof, err = generateCensusProof(cli, censusRoot, vote.Address)
+		if err != nil {
+			log.Errorw(err, "failed to generate census proof")
+			return
+		}
+		log.Infow("census proof generated", "proof", vote.CensusProof)
+
+		// Send the vote to the sequencer
+		voteID, err := sendVote(cli, vote)
+		if err != nil {
+			log.Errorw(err, "failed to send vote")
+			return
+		}
+		log.Infow("vote sent",
+			"voteID", voteID.String(),
+			"currentVote", i+1,
+			"totalVotes", *voteCount)
+
+		// Wait the voteSleepTime before sending the next vote
+		time.Sleep(*voteSleepTime)
+	}
+
+	// Wait for the votes to be registered in the smart contract
+	log.Info("all votes sent, waiting for votes to be registered in smart contract...")
+	voteCountCh := make(chan int)
+	voteCountCtx, cancel := context.WithCancel(testCtx)
+	defer cancel()
+	go func() {
+		for registeredVotes := range voteCountCh {
+			log.Infow("votes registered in smart contract", "registeredVotes", registeredVotes)
+			if registeredVotes >= *voteCount {
+				cancel()
+				break
+			}
+		}
+	}()
+	if err := listenSmartContractVoteCount(voteCountCtx, contracts, pid, voteCountCh); err != nil {
+		log.Errorw(err, "failed to wait for votes to be registered in smart contract")
+		return
+	}
+	log.Info("all votes registered in smart contract")
+}
+
+func startLocalSequencer(ctx context.Context, contracts *web3.Contracts) error {
+	// Create storage with a in-memory database
+	stg := storage.New(memdb.New())
+	sequencer.AggregatorTickerInterval = time.Second * 2
+	sequencer.NewProcessMonitorInterval = time.Second * 5
+	vp := service.NewSequencer(stg, contracts, time.Second*30)
+	if err := vp.Start(ctx); err != nil {
+		return fmt.Errorf("failed to start sequencer: %v", err)
+	}
+	// Monitor new processes from the contracts
+	pm := service.NewProcessMonitor(contracts, stg, time.Second*2)
+	if err := pm.Start(ctx); err != nil {
+		return fmt.Errorf("failed to start process monitor: %v", err)
+	}
+	// Start finalizer service
+	fin := service.NewFinalizer(stg, stg.StateDB(), time.Second*5)
+	if err := fin.Start(ctx, time.Second*5); err != nil {
+		return fmt.Errorf("failed to start finalizer: %v", err)
+	}
+	// Start API service
+	api := service.NewAPI(stg, defaultSequencerHost, defaultSequencerPort, defaultNetwork)
+	if err := api.Start(ctx); err != nil {
+		return fmt.Errorf("failed to start API: %v", err)
+	}
+	return nil
 }
 
 func createOrganization(contracts *web3.Contracts) (common.Address, error) {
@@ -215,19 +275,19 @@ func createOrganization(contracts *web3.Contracts) (common.Address, error) {
 	return orgAddr, nil
 }
 
-func createCensus(cli *client.HTTPclient, size int) ([]byte, []*api.CensusParticipant, []*ethereum.Signer, error) {
+func createCensus(cli *client.HTTPclient, size int) ([]byte, []*ethereum.Signer, error) {
 	// Request a new census
 	body, code, err := cli.Request(http.MethodPost, nil, nil, api.NewCensusEndpoint)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to request new census: %v", err)
+		return nil, nil, fmt.Errorf("failed to request new census: %v", err)
 	} else if code != http.StatusOK {
-		return nil, nil, nil, fmt.Errorf("failed to request new census, status code: %d", code)
+		return nil, nil, fmt.Errorf("failed to request new census, status code: %d", code)
 	}
 
 	// Decode census response
 	var resp api.NewCensus
 	if err := json.NewDecoder(bytes.NewReader(body)).Decode(&resp); err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to decode census response: %v", err)
+		return nil, nil, fmt.Errorf("failed to decode census response: %v", err)
 	}
 
 	// Generate random participants
@@ -236,7 +296,7 @@ func createCensus(cli *client.HTTPclient, size int) ([]byte, []*api.CensusPartic
 	for range size {
 		signer, err := ethereum.NewSigner()
 		if err != nil {
-			return nil, nil, nil, fmt.Errorf("failed to create signer: %v", err)
+			return nil, nil, fmt.Errorf("failed to create signer: %v", err)
 		}
 		censusParticipants.Participants = append(censusParticipants.Participants, &api.CensusParticipant{
 			Key:    signer.Address().Bytes(),
@@ -249,27 +309,27 @@ func createCensus(cli *client.HTTPclient, size int) ([]byte, []*api.CensusPartic
 	addEnpoint := api.EndpointWithParam(api.AddCensusParticipantsEndpoint, api.CensusURLParam, resp.Census.String())
 	_, code, err = cli.Request(http.MethodPost, censusParticipants, nil, addEnpoint)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to add participants to census: %v", err)
+		return nil, nil, fmt.Errorf("failed to add participants to census: %v", err)
 	} else if code != http.StatusOK {
-		return nil, nil, nil, fmt.Errorf("failed to add participants to census, status code: %d", code)
+		return nil, nil, fmt.Errorf("failed to add participants to census, status code: %d", code)
 	}
 
 	// Get census root
 	getRootEnpoint := api.EndpointWithParam(api.GetCensusRootEndpoint, api.CensusURLParam, resp.Census.String())
 	body, code, err = cli.Request(http.MethodGet, nil, nil, getRootEnpoint)
 	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to get census root: %v", err)
+		return nil, nil, fmt.Errorf("failed to get census root: %v", err)
 	} else if code != http.StatusOK {
-		return nil, nil, nil, fmt.Errorf("failed to get census root, status code: %d", code)
+		return nil, nil, fmt.Errorf("failed to get census root, status code: %d", code)
 	}
 
 	// Decode census root
 	var rootResp api.CensusRoot
 	if err := json.NewDecoder(bytes.NewReader(body)).Decode(&rootResp); err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to decode census root response: %v", err)
+		return nil, nil, fmt.Errorf("failed to decode census root response: %v", err)
 	}
 
-	return rootResp.Root, censusParticipants.Participants, signers, nil
+	return rootResp.Root, signers, nil
 }
 
 func createProcess(
@@ -357,4 +417,161 @@ func createProcess(
 		}
 	}
 	return pid, encryptionKeys, nil
+}
+
+func createVote(
+	privKey *ethereum.Signer,
+	pid *types.ProcessID,
+	encKey *types.EncryptionKey,
+	bm *types.BallotMode,
+) (api.Vote, error) {
+	// Emulate user inputs
+	address := ethcrypto.PubkeyToAddress(privKey.PublicKey)
+	secret := util.RandomBytes(16)
+	k, err := elgamal.RandK()
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to generate random k: %v", err)
+	}
+
+	// Generate random ballot fields
+	randFields := ballotprooftest.GenBallotFieldsForTest(
+		int(bm.MaxCount),
+		int(bm.MaxValue.MathBigInt().Int64()),
+		int(bm.MinValue.MathBigInt().Int64()),
+		bm.ForceUniqueness)
+
+	// Cast fields to types.BigInt
+	fields := []*types.BigInt{}
+	for _, f := range randFields {
+		fields = append(fields, (*types.BigInt)(f))
+	}
+
+	// Compose wasm inputs
+	wasmInputs := &ballotproof.BallotProofInputs{
+		Address:   address.Bytes(),
+		ProcessID: pid.Marshal(),
+		Secret:    secret,
+		EncryptionKey: []*types.BigInt{
+			(*types.BigInt)(encKey.X),
+			(*types.BigInt)(encKey.Y),
+		},
+		K:           (*types.BigInt)(k),
+		BallotMode:  bm,
+		Weight:      (*types.BigInt)(new(big.Int).SetUint64(circuits.MockWeight)),
+		FieldValues: fields,
+	}
+
+	// Generate the inputs for the ballot proof circuit
+	wasmResult, err := ballotproof.GenerateBallotProofInputs(wasmInputs)
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to generate ballot proof inputs: %v", err)
+	}
+
+	// Encode the inputs to json
+	encodedCircomInputs, err := json.Marshal(wasmResult.CircomInputs)
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to encode circom inputs: %v", err)
+	}
+
+	// Generate the proof using the circom circuit
+	rawProof, pubInputs, err := ballotprooftest.CompileAndGenerateProofForTest(encodedCircomInputs)
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to generate proof: %v", err)
+	}
+
+	// Convert the proof to gnark format
+	circomProof, _, err := circuits.Circom2GnarkProof(rawProof, pubInputs)
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to convert proof to gnark format: %v", err)
+	}
+
+	// Sign the hash of the circuit inputs
+	signature, err := ballotprooftest.SignECDSAForTest(privKey, wasmResult.VoteID)
+	if err != nil {
+		return api.Vote{}, fmt.Errorf("failed to sign vote: %v", err)
+	}
+
+	// Return the vote ready to be sent to the sequencer
+	return api.Vote{
+		ProcessID:        wasmResult.ProccessID,
+		Address:          wasmInputs.Address,
+		Commitment:       wasmResult.Commitment,
+		Nullifier:        wasmResult.Nullifier,
+		Ballot:           wasmResult.Ballot,
+		BallotProof:      circomProof,
+		BallotInputsHash: wasmResult.BallotInputsHash,
+		Signature:        signature.Bytes(),
+	}, nil
+}
+
+func generateCensusProof(cli *client.HTTPclient, root []byte, key []byte) (types.CensusProof, error) {
+	// Get proof for the key
+	getProofEnpoint := api.EndpointWithParam(api.GetCensusProofEndpoint, api.CensusURLParam, hex.EncodeToString(root))
+	body, code, err := cli.Request(http.MethodGet, nil, []string{"key", hex.EncodeToString(key)}, getProofEnpoint)
+	if err != nil {
+		return types.CensusProof{}, fmt.Errorf("failed to get census proof: %v", err)
+	} else if code != http.StatusOK {
+		return types.CensusProof{}, fmt.Errorf("failed to get census proof, status code: %d", code)
+	}
+
+	// Decode proof response
+	var proof types.CensusProof
+	if err := json.NewDecoder(bytes.NewReader(body)).Decode(&proof); err != nil {
+		return types.CensusProof{}, fmt.Errorf("failed to decode census proof response: %v", err)
+	}
+
+	return proof, nil
+}
+
+func sendVote(cli *client.HTTPclient, vote api.Vote) (types.HexBytes, error) {
+	// Make the request to cast the vote
+	body, status, err := cli.Request(http.MethodPost, vote, nil, api.VotesEndpoint)
+	if err != nil {
+		return nil, fmt.Errorf("failed to cast vote: %v", err)
+	} else if status != http.StatusOK {
+		return nil, fmt.Errorf("failed to cast vote, status code: %d", status)
+	}
+
+	// Parse the response body to get the vote ID
+	var voteResponse api.VoteResponse
+	if err := json.NewDecoder(bytes.NewReader(body)).Decode(&voteResponse); err != nil {
+		return nil, fmt.Errorf("failed to decode vote response: %v", err)
+	}
+
+	return voteResponse.VoteID, nil
+}
+
+func listenSmartContractVoteCount(
+	ctx context.Context,
+	contracts *web3.Contracts,
+	pid *types.ProcessID,
+	voteCountCh chan int,
+) error {
+	ticker := time.NewTicker(time.Second * 30)
+	lastVoteCount := -1
+	for {
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("process creation timeout: %v", ctx.Err())
+		case <-ticker.C:
+			process, err := contracts.Process(pid.Marshal())
+			if err != nil {
+				return fmt.Errorf("failed to get process: %v", err)
+			}
+			if process == nil {
+				return fmt.Errorf("process not found")
+			}
+			// Get the vote count from the process, by default it is 0
+			var voteCount int
+			if process.VoteCount != nil {
+				voteCount = int(process.VoteCount.MathBigInt().Int64())
+			}
+			// If the vote count has changed, send it to the channel and update
+			// the last vote count
+			if voteCount != lastVoteCount {
+				lastVoteCount = voteCount
+				voteCountCh <- voteCount
+			}
+		}
+	}
 }
