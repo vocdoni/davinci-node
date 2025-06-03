8ce1a1b8ef72e43a9e2e6fc164507f3f4ddd2dea
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2025-06-03 10:13:25 +0100

    web3: bump contracts

--- config/contracts.go
+++ config/contracts.go
@@ -11,10 +11,10 @@ type DavinciWeb3Config struct {
 // DefaultConfig contains the default smart contract addresses for Davinci by network.
 var DefaultConfig = map[string]DavinciWeb3Config{
 	"sep": {
-		ProcessRegistrySmartContract:      "0xB538d3fBF4C9cF3A31d6c6E2d15B405Ff66c3B15",
-		OrganizationRegistrySmartContract: "0x159630c26381AB98867E4A3f631fB69fE6b48DBF",
-		ResultsZKVerifier:                 "0xE2046392a389795Fe6F2b15C8A8Fc1582554828E", // duplicate for now
-		StateTransitionZKVerifier:         "0x13249EF15BEa50736b46BF8fF4d2DC2b4B32F151",
+		ProcessRegistrySmartContract:      "0xd721ACDc64758F8eCb0ae016C7aE563509D9Ac6F",
+		OrganizationRegistrySmartContract: "0x9F354dce18279F6bC3b17A5df78CbD3536e3f18c",
+		ResultsZKVerifier:                 "0xEE95EbD00BFA339BcCE28011Ee7e4D0377C22dDB",
+		StateTransitionZKVerifier:         "0xdD1e78b5Ec6ADB7D3C9243FE42fD9180e9Cc8F43",
 	},
 }
 
