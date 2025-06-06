44e3df1a06582dd921492d2870947bf24411f856
p4u <pau@dabax.net>
2025-06-06 09:16:15 +0200

    add debug traces

--- storage/process_stats.go
+++ storage/process_stats.go
@@ -29,9 +29,20 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 		return fmt.Errorf("failed to get process for stats update: %w", err)
 	}
 
+	// DEBUG: Log the current state before any updates
+	log.Infow("DEBUG: updateProcessStats BEFORE",
+		"processID", fmt.Sprintf("%x", pid),
+		"verifiedVotes", p.SequencerStats.VerifiedVotesCount,
+		"pendingVotes", p.SequencerStats.PendingVotesCount,
+		"currentBatchSize", p.SequencerStats.CurrentBatchSize,
+		"aggregatedVotes", p.SequencerStats.AggregatedVotesCount,
+		"updates", fmt.Sprintf("%+v", updates),
+	)
+
 	for _, update := range updates {
 		switch update.TypeStats {
 		case types.TypeStatsStateTransitions:
+			oldValue := p.SequencerStats.StateTransitionCount
 			newValue := p.SequencerStats.StateTransitionCount + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative StateTransitionCount, clamping to 0",
@@ -43,7 +54,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.StateTransitionCount = newValue
 			}
+			log.Infow("DEBUG: StateTransitionCount update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.StateTransitionCount,
+			)
 		case types.TypeStatsSettledStateTransitions:
+			oldValue := p.SequencerStats.SettledStateTransitionCount
 			newValue := p.SequencerStats.SettledStateTransitionCount + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative SettledStateTransitionCount, clamping to 0",
@@ -55,7 +73,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.SettledStateTransitionCount = newValue
 			}
+			log.Infow("DEBUG: SettledStateTransitionCount update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.SettledStateTransitionCount,
+			)
 		case types.TypeStatsAggregatedVotes:
+			oldValue := p.SequencerStats.AggregatedVotesCount
 			newValue := p.SequencerStats.AggregatedVotesCount + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative AggregatedVotesCount, clamping to 0",
@@ -67,7 +92,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.AggregatedVotesCount = newValue
 			}
+			log.Infow("DEBUG: AggregatedVotesCount update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.AggregatedVotesCount,
+			)
 		case types.TypeStatsVerifiedVotes:
+			oldValue := p.SequencerStats.VerifiedVotesCount
 			newValue := p.SequencerStats.VerifiedVotesCount + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative VerifiedVotesCount, clamping to 0",
@@ -79,7 +111,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.VerifiedVotesCount = newValue
 			}
+			log.Infow("DEBUG: VerifiedVotesCount update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.VerifiedVotesCount,
+			)
 		case types.TypeStatsPendingVotes:
+			oldValue := p.SequencerStats.PendingVotesCount
 			newValue := p.SequencerStats.PendingVotesCount + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative PendingVotesCount, clamping to 0",
@@ -91,7 +130,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.PendingVotesCount = newValue
 			}
+			log.Infow("DEBUG: PendingVotesCount update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.PendingVotesCount,
+			)
 		case types.TypeStatsLastBatchSize:
+			oldValue := p.SequencerStats.LastBatchSize
 			if update.Delta < 0 {
 				log.Warnw("attempted to set negative LastBatchSize, clamping to 0",
 					"processID", fmt.Sprintf("%x", pid),
@@ -101,7 +147,14 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.LastBatchSize = update.Delta
 			}
+			log.Infow("DEBUG: LastBatchSize update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.LastBatchSize,
+			)
 		case types.TypeStatsCurrentBatchSize:
+			oldValue := p.SequencerStats.CurrentBatchSize
 			newValue := p.SequencerStats.CurrentBatchSize + update.Delta
 			if newValue < 0 {
 				log.Warnw("attempted to set negative CurrentBatchSize, clamping to 0",
@@ -113,10 +166,25 @@ func (s *Storage) updateProcessStats(pid []byte, updates []ProcessStatsUpdate) e
 			} else {
 				p.SequencerStats.CurrentBatchSize = newValue
 			}
+			log.Infow("DEBUG: CurrentBatchSize update",
+				"processID", fmt.Sprintf("%x", pid),
+				"oldValue", oldValue,
+				"delta", update.Delta,
+				"newValue", p.SequencerStats.CurrentBatchSize,
+			)
 		default:
 			return fmt.Errorf("unknown type stats: %d", update.TypeStats)
 		}
 	}
+
+	// DEBUG: Log the final state after all updates
+	log.Infow("DEBUG: updateProcessStats AFTER",
+		"processID", fmt.Sprintf("%x", pid),
+		"verifiedVotes", p.SequencerStats.VerifiedVotesCount,
+		"pendingVotes", p.SequencerStats.PendingVotesCount,
+		"currentBatchSize", p.SequencerStats.CurrentBatchSize,
+		"aggregatedVotes", p.SequencerStats.AggregatedVotesCount,
+	)
 
 	if err := s.setArtifact(processPrefix, pid, p); err != nil {
 		return fmt.Errorf("failed to save process after stats update: %w", err)
