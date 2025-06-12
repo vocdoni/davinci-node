9d9e1de5ccf687771a65cf0d4424b07335c8faf4
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2025-06-12 14:30:16 +0100

    bump contracts, add uzh network

--- config/contracts.go
+++ config/contracts.go
@@ -16,9 +16,16 @@ var DefaultConfig = map[string]DavinciWeb3Config{
 		ResultsZKVerifier:                 "0x6084fe520e7c6616be1C75713F63c5bBc31046FE",
 		StateTransitionZKVerifier:         "0x40EFFAC7c37Dfc162F92CBcE4E16ACbF8D695A72",
 	},
+	"uzh": {
+		ProcessRegistrySmartContract:      "0xBC1A75100023add2E798f16790704372E2a36085",
+		OrganizationRegistrySmartContract: "0x4102a669FAAD42e6202b2c7bF5d6C5aB0F722217",
+		ResultsZKVerifier:                 "0xf6Fb4C1348dEBf52f7D5Fa59B920Ac403dbc4627",
+		StateTransitionZKVerifier:         "0xbae94CAFe76bAEcA335a8a338483957BB1E85Eb5",
+	},
 }
 
 // AvailableNetworks contains the list of networks where Davinci is deployed.
 var AvailableNetworks = []string{
 	"sep",
+	"uzh",
 }
