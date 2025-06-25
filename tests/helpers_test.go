6a6c19bda16355607ed69ab818fa2980f32de7fc
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2025-06-25 18:27:05 +0200

    change default branch test

--- tests/helpers_test.go
+++ tests/helpers_test.go
@@ -102,7 +102,7 @@ func setupWeb3(t *testing.T, ctx context.Context) *web3.Contracts {
 		composeEnv[anvilPortEnvVarName] = fmt.Sprintf("%d", anvilPort)
 		composeEnv[deployerServerPortEnvVarName] = fmt.Sprintf("%d", anvilPort+1)
 		composeEnv[privKeyEnvVarName] = testLocalAccountPrivKey
-		composeEnv[zContractsBranchNameEnvVarName] = "f/results_status"
+		composeEnv[zContractsBranchNameEnvVarName] = "main"
 
 		// Create docker-compose instance
 		compose, err := tc.NewDockerCompose("docker/docker-compose.yml")
