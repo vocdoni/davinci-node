7fbb77e1861f671b46c7eb08d598835262a8ad30
Lucas Menéndez <hi@lucasmenendez.me>
2025-08-22 13:03:59 +0200

    fix: Internal marshal nested structs (#186)
    * new storage encode functions to support multiple formats like cbor or json
    * support to define the artifact format encoding in storage basic functions
    * use json encoding to internally storing metadata objects
    * encode tests

--- storage/helpers.go
+++ storage/helpers.go
@@ -1,26 +1,6 @@
 package storage
 
-import (
-	"crypto/sha256"
-	"fmt"
-
-	"github.com/fxamacker/cbor/v2"
-)
-
-// EncodeArtifact encodes an artifact into CBOR format.
-func EncodeArtifact(a any) ([]byte, error) {
-	encOpts := cbor.CoreDetEncOptions()
-	em, err := encOpts.EncMode()
-	if err != nil {
-		return nil, fmt.Errorf("encode artifact: %w", err)
-	}
-	return em.Marshal(a)
-}
-
-// DecodeArtifact decodes a CBOR-encoded artifact into the provided output variable.
-func DecodeArtifact(data []byte, out any) error {
-	return cbor.Unmarshal(data, out)
-}
+import "crypto/sha256"
 
 func hashKey(data []byte) []byte {
 	hash := sha256.Sum256(data)
