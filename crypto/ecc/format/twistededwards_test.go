20bf5fc4de3b80d6b9667bb509976efffecbbacd
Guido Iribarren <git@guidoi.com.ar>
2026-01-27 09:34:48 +0100

    test(crypto): add TestRTEtoTEVarTransform and TestEmulatedRTEtoTETransform

--- crypto/ecc/format/twistededwards_test.go
+++ crypto/ecc/format/twistededwards_test.go
@@ -3,6 +3,13 @@ package format
 import (
 	"math/big"
 	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/backend"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/test"
 )
 
 func TestTE2RTETransform(t *testing.T) {
@@ -22,4 +29,84 @@ func TestTE2RTETransform(t *testing.T) {
 	} else if yPrimePrime.Cmp(y) != 0 {
 		t.Errorf("Expected %v, got %v", y, yPrimePrime)
 	}
+}
+
+type rteToTEVarCircuit struct {
+	XRTE      frontend.Variable `gnark:",public"`
+	YRTE      frontend.Variable `gnark:",public"`
+	ExpectedX frontend.Variable
+	ExpectedY frontend.Variable
+}
+
+func (circuit *rteToTEVarCircuit) Define(api frontend.API) error {
+	xTE, yTE := FromRTEtoTEVar(api, circuit.XRTE, circuit.YRTE)
+	api.AssertIsEqual(xTE, circuit.ExpectedX)
+	api.AssertIsEqual(yTE, circuit.ExpectedY)
+	xRTE, yRTE := FromTEtoRTEVar(api, xTE, yTE)
+	api.AssertIsEqual(xRTE, circuit.XRTE)
+	api.AssertIsEqual(yRTE, circuit.YRTE)
+	return nil
+}
+
+func TestRTEtoTEVarTransform(t *testing.T) {
+	x, _ := new(big.Int).SetString("20284931487578954787250358776722960153090567235942462656834196519767860852891", 10)
+	y, _ := new(big.Int).SetString("21185575020764391300398134415668786804224896114060668011215204645513129497221", 10)
+
+	xRTE, yRTE := FromTEtoRTE(x, y)
+
+	witness := rteToTEVarCircuit{
+		XRTE:      xRTE,
+		YRTE:      yRTE,
+		ExpectedX: x,
+		ExpectedY: y,
+	}
+
+	assert := test.NewAssert(t)
+	assert.SolvingSucceeded(&rteToTEVarCircuit{}, &witness,
+		test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
+}
+
+type emulatedRTEtoTECircuit struct {
+	XRTE      emulated.Element[sw_bn254.ScalarField] `gnark:",public"`
+	YRTE      emulated.Element[sw_bn254.ScalarField] `gnark:",public"`
+	ExpectedX emulated.Element[sw_bn254.ScalarField]
+	ExpectedY emulated.Element[sw_bn254.ScalarField]
+}
+
+func (circuit *emulatedRTEtoTECircuit) Define(api frontend.API) error {
+	xTE, yTE, err := FromEmulatedRTEtoTE(api, circuit.XRTE, circuit.YRTE)
+	if err != nil {
+		return err
+	}
+	field, err := emulated.NewField[sw_bn254.ScalarField](api)
+	if err != nil {
+		return err
+	}
+	field.AssertIsEqual(&xTE, &circuit.ExpectedX)
+	field.AssertIsEqual(&yTE, &circuit.ExpectedY)
+	xRTE, yRTE, err := FromEmulatedTEtoRTE(api, xTE, yTE)
+	if err != nil {
+		return err
+	}
+	field.AssertIsEqual(&xRTE, &circuit.XRTE)
+	field.AssertIsEqual(&yRTE, &circuit.YRTE)
+	return nil
+}
+
+func TestEmulatedRTEtoTETransform(t *testing.T) {
+	x, _ := new(big.Int).SetString("20284931487578954787250358776722960153090567235942462656834196519767860852891", 10)
+	y, _ := new(big.Int).SetString("21185575020764391300398134415668786804224896114060668011215204645513129497221", 10)
+
+	xRTE, yRTE := FromTEtoRTE(x, y)
+
+	witness := emulatedRTEtoTECircuit{
+		XRTE:      emulated.ValueOf[sw_bn254.ScalarField](xRTE),
+		YRTE:      emulated.ValueOf[sw_bn254.ScalarField](yRTE),
+		ExpectedX: emulated.ValueOf[sw_bn254.ScalarField](x),
+		ExpectedY: emulated.ValueOf[sw_bn254.ScalarField](y),
+	}
+
+	assert := test.NewAssert(t)
+	assert.SolvingSucceeded(&emulatedRTEtoTECircuit{}, &witness,
+		test.WithCurves(ecc.BN254), test.WithBackends(backend.GROTH16))
 }
