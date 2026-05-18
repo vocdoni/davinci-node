b3d6dfdb380c5e8d903b4050b593045e116e57ff
Lucas Menendez <hi@lucasmenendez.me>
2026-05-18 07:29:39 +0000

    test(all): fix and reenable skipped tests
    fix: storage/keys_test.go - TestEncryptionKeys
    
    fix: state/blob_test.go - TestParseBlobData_FromFile
    
    fix: sequencer/finalizer_test.go - TestFinalize & TestFinalizeMissingEncryptionKeysReturnsSequencerSentinel
    
    fix: service/state_sync_test.go - TestStateSync
    
    fix: service/process_monitor_test.go - TestProcessMonitor

--- storage/keys_test.go
+++ storage/keys_test.go
@@ -11,10 +11,10 @@ import (
 	"github.com/vocdoni/davinci-node/db"
 	"github.com/vocdoni/davinci-node/db/metadb"
 	"github.com/vocdoni/davinci-node/internal/testutil"
+	"github.com/vocdoni/davinci-node/types"
 )
 
 func TestEncryptionKeys(t *testing.T) {
-	t.Skip("TODO: fix and re-enable")
 	c := qt.New(t)
 	tempDir := t.TempDir()
 	dbPath := filepath.Join(tempDir, "db")
@@ -39,18 +39,26 @@ func TestEncryptionKeys(t *testing.T) {
 	publicKey1, privateKey1, err := elgamal.GenerateKey(bjj.New())
 	c.Assert(err, qt.IsNil)
 
+	p1x1, p1y1 := publicKey1.Point()
+	process1 := testutil.RandomProcessWithEncryptionKey(processID1, types.EncryptionKey{
+		X: (*types.BigInt)(p1x1),
+		Y: (*types.BigInt)(p1y1),
+	})
+
+	err = st.NewProcess(process1)
+	c.Assert(err, qt.IsNil)
+
 	// Test 2: Store encryption keys for processID1
-	t.Log("TODO: fix SetEncryptionKeys", processID1, publicKey1, privateKey1)
-	err = st.setEncryptionKeysUnsafe(publicKey1, privateKey1)
+	err = st.SetEncryptionKeys(publicKey1, privateKey1)
 	c.Assert(err, qt.IsNil)
 
 	// Verify we can retrieve the keys
 	pubKey, privKey, err := st.ProcessEncryptionKeys(processID1)
 	c.Assert(err, qt.IsNil)
-	x1, y1 := publicKey1.Point()
-	x2, y2 := pubKey.Point()
-	c.Assert(x1.Cmp(x2), qt.Equals, 0)
-	c.Assert(y1.Cmp(y2), qt.Equals, 0)
+
+	p1x2, p1y2 := pubKey.Point()
+	c.Assert(p1x1.Cmp(p1x2), qt.Equals, 0)
+	c.Assert(p1y1.Cmp(p1y2), qt.Equals, 0)
 	c.Assert(privateKey1.Cmp(privKey), qt.Equals, 0)
 
 	// Test 3: List encryption keys with one process
@@ -63,8 +71,16 @@ func TestEncryptionKeys(t *testing.T) {
 	publicKey2, privateKey2, err := elgamal.GenerateKey(bjj.New())
 	c.Assert(err, qt.IsNil)
 
+	p2x1, p2y1 := publicKey2.Point()
+	process2 := testutil.RandomProcessWithEncryptionKey(processID2, types.EncryptionKey{
+		X: (*types.BigInt)(p2x1),
+		Y: (*types.BigInt)(p2y1),
+	})
+
+	err = st.NewProcess(process2)
+	c.Assert(err, qt.IsNil)
+
 	// Test 4: Store encryption keys for processID2
-	t.Log("TODO: fix SetEncryptionKeys", processID2, publicKey2, privateKey2)
 	err = st.setEncryptionKeysUnsafe(publicKey2, privateKey2)
 	c.Assert(err, qt.IsNil)
 
