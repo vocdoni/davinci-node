3d9859186ea9061b595e4be5faf8b15faaafd69f
Guido Iribarren <git@guidoi.com.ar>
2026-05-26 12:09:03 +0200

    chore!(circuits): recompile artifacts

--- config/circuit_artifacts.go
+++ config/circuit_artifacts.go
@@ -13,21 +13,21 @@ const (
 
 // Hashes of each circuit artifacts
 const (
-	BallotProofCircuitHash         = "fb0856e46cb630114f7068010f1ab4edcb43a2a491e56518dba0102cfd6926be"
-	BallotProofProvingKeyHash      = "f5809dd04a10a1546a1a80276ed27881ebc9bfb0272c2cac6a03abf11b5543d4"
-	BallotProofVerificationKeyHash = "d45aa6c83b8df847e9c7bbd0cce299c791ce18a2203473e9a1a89e73818e85c6"
+	BallotProofCircuitHash         = "5533928fc3a028f677a6dab2e9b02df7d6d7b9c0fed334f36a500bf36678c10a"
+	BallotProofProvingKeyHash      = "f4a9ad3537ebf9295d1409b8ce3ebb49e80247526956bfdb04d80ebc5fed58f7"
+	BallotProofVerificationKeyHash = "fb94a56b5b1e90df5ae7f22ea60e836792a6a5d5b50382f6faa154df0175778e"
 
-	VoteVerifierCircuitHash         = "28f60015e6f5da5f54b5bda40f7bc47b50d3299926a06cc48296e52baf4cfad0"
-	VoteVerifierProvingKeyHash      = "258a94202aefdfa42ec8a89840faf5b8ce881d7192685fafd7b66c57cbc2bbdc"
-	VoteVerifierVerificationKeyHash = "2cd6f8735ea6fdfed83f1c5b50c0d13fb7f6569a15682fd055f49cdd37dfadf9"
+	VoteVerifierCircuitHash         = "5e955e7a63f299648ceaa2026dec7348a441bd1738eef1ec01a221d236a9414d"
+	VoteVerifierProvingKeyHash      = "9d17ca05c9ee797be4ca2360577ab67531f21906915e87bdc41f001d1ee53e77"
+	VoteVerifierVerificationKeyHash = "8ccc7f7632867f79df8ad6943c7ec281b705434e6d7a1e81e9d85b31e617a295"
 
-	AggregatorCircuitHash         = "ef3f700df475fa2abf546f99037684a7530bcb583812ff61df5cc889edf49fbb"
-	AggregatorProvingKeyHash      = "8b62de5d10623e0331d3ed39bd2d5f36e9311fbf0680226cd8fdd04ad13c5188"
-	AggregatorVerificationKeyHash = "44ce44014f7fe042ea21ca58416f2ad90ecea476f5ddae321cde0a6643f44852"
+	AggregatorCircuitHash         = "35a819b2eebabf5ae2a7d347455729d5ae5ef94a718a34bd9ef85c965af36e2e"
+	AggregatorProvingKeyHash      = "b66840854f4896c5f617d3ff9484701dccde91702039b475c9c028b494f28830"
+	AggregatorVerificationKeyHash = "c5d03e93b49500f128038635156745bd3b39c44e6e5705cffc7a9606d7c56529"
 
-	StateTransitionCircuitHash         = "659d38c3aa3001e3297ecd135fd7e9fbd52e69f10c0222459e6ca1fb4ec16512"
-	StateTransitionProvingKeyHash      = "d8d1400e7922b6eb7652caf13546156b915a9565cfff6130365be103bedd6e68"
-	StateTransitionVerificationKeyHash = "59b0a5a7d6eb0145e0c08b07a11ec0496480cd13e908ad6be593d6141046623d"
+	StateTransitionCircuitHash         = "0029249847b7094d3b745139ee1bd4f8d065a6a684dc32cbfe8b786022a113c3"
+	StateTransitionProvingKeyHash      = "a71fc6c6c3f684e735f7d47fea5710ce12ce23a41b1b1c61cd1c1fc91a6cf31f"
+	StateTransitionVerificationKeyHash = "44f938269dfbbd75570c4eb0785e14b2361693c3de78038d1c3d4ddb0e3456d1"
 
 	ResultsVerifierCircuitHash         = "386646c4ab455b71afa2bd8a8f03e3ad1913e81972c8eb4a14455393846c00a3"
 	ResultsVerifierProvingKeyHash      = "448592882f39f7e5ef17ad70cdee5a23f95b1337ec72ae1ac36266a306cc6bea"
